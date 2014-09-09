2ab98dd3db6643bdaa48ff2eb284bd276bc16a45
coolpie <saomqun@gmail.com>
2014-09-09 23:37:03 +0800

    value

--- interpreter/value.h
+++ interpreter/value.h
@@ -2,12 +2,24 @@
 #ifndef _VALUE_H_
 #define _VALUE_H_
 
+#include <string>
 
 class Value
 {
 public:
 	Value();
 	~Value();
+	/* Get value of this. */
+	void getValue();
+
+private:
+	std::string nativeStr;
+	int type;
+	std::string getStringValue();
+	int getIntValue();
+	int getBooleanValue();
+	std::string getNullValue();
+	std::string getUndefineValue();
 };
 
 #endif
