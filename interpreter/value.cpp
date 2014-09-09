3e8f63c64acf82fdee0cf2ed842e212bf5575290
coolpie <saomqun@gmail.com>
2014-09-09 23:24:29 +0800

    define value

---
+++ interpreter/value.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "value.h"
+
+
+Value::Value()
+{
+}
+
+
+Value::~Value()
+{
+}
---
+++ interpreter/value.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _VALUE_H_
+#define _VALUE_H_
+
+
+class Value
+{
+public:
+	Value();
+	~Value();
+};
+
+#endif
